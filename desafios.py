abd234bac442f57ee08451bccd16b46090887ab1
hayssa cunha <hayssa.cunhaa@fmail.com>
2025-11-05 22:07:50 -0400

    feat: add function mostrar mensagem

--- desafios.py
+++ desafios.py
@@ -18,7 +18,7 @@ def mostrar_mensagem_inicial():
     Exibe uma mensagem de boas-vindas ao desafio.
     Retorno esperado: string com a mensagem "Bem-vindo ao Desafio de Git!"
     """
-    pass
+    print("Bem-vindo ao Desafio de Git!")
 
 def listar_comandos_git_basicos():
     """
@@ -58,3 +58,5 @@ def gerar_relatorio_final(funcoes_concluidas):
     "Desafio concluído! 2 funções implementadas com sucesso."
     """
     pass
+
+mostrar_mensagem_inicial()
